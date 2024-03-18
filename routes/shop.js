0490b313dbbb70055027604cb990777a4e934945
Akbar Rahmat Mulyatama <88664674+akbarrahmatm@users.noreply.github.com>
2024-03-18 23:10:59 +0700

    Feature : refactoring shop.html to shop.ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,17 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+router.get("/", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "shop.ejs"));
+  const products = adminData.products;
+  console.log(products);
+  res.render("shop", { title: "this is ejs", products: products });
 });
 
 module.exports = router;
